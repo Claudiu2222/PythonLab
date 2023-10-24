daf13ecea1ae9c035955299f1ca6ec4ed712ad88
Claudiu Popoiu <54360338+Claudiu2222@users.noreply.github.com>
2023-10-24 16:39:51 +0300

    Fixed ex3

--- Week2/ex3.py
+++ Week2/ex3.py
@@ -1,16 +1,21 @@
 def operations_for_list(list_one, list_two):
-    set_one = set(list_one)
-    set_two = set(list_two)
+    # set_one = set(list_one)
+    # set_two = set(list_two)
 
-    intersection = set_one.intersection(set_two)
-    reunion = set_one.union(set_two)
-    difference_first_second = set_one.difference(set_two)
-    difference_second_first = set_two.difference(set_one)
+    # intersection = set_one.intersection(set_two)
+    # reunion = set_one.union(set_two)
+    # difference_first_second = set_one.difference(set_two)
+    # difference_second_first = set_two.difference(set_one)
 
-    return (intersection, reunion, difference_first_second, difference_second_first)
+    intersection = [i for i in list_one if i in list_two]
+    difference_first_second = [i for i in list_one if i not in list_two]
+    differnece_second_first = [i for i in list_two if i not in list_one]
+    reunion = intersection + difference_first_second + differnece_second_first
+
+    return (intersection, difference_first_second, differnece_second_first,reunion)
 
 
-list_one = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-list_two = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 200]
+list_one = [1, 2, 3, 4,4, 5, 6, 7, 8, 9, 10]
+list_two = [5, 6, 7, 8, 9,4, 10, 11, 12, 13, 14, 15, 16, 17, 200]
 
 print(operations_for_list(list_one, list_two))
