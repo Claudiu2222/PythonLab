03e01c9becd30648ac9902ac0dd9fde9ac057671
Claudiu Popoiu <54360338+Claudiu2222@users.noreply.github.com>
2023-12-10 23:13:15 +0200

    Basic window creation

--- Project/gui.py
+++ Project/gui.py
@@ -1,5 +1,9 @@
 import tkinter as tk
 from tkinter import ttk
 
-class GuiApiClient():
-    
+class GuiApiClient(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("API Client")
+        self.geometry("500x500")
+        self.resizable(False, False)
--- Project/main.py
+++ Project/main.py
@@ -1,1 +1,5 @@
-#
+from gui import GuiApiClient
+
+if __name__ == '__main__':
+    my_gui = GuiApiClient()
+    my_gui.mainloop()
